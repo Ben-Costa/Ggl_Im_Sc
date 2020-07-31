ac6057341374f4d83f072d9b23ca462720126a80
Ben-Costa <bc234cc@gmail.comm>
2020-07-31 17:49:23 -0500

    Added get_URLs, retrieve_Images, and save Images

--- Google_Image_Scraper_v1.py
+++ Google_Image_Scraper_v1.py
@@ -23,11 +23,121 @@ def get_queueries():
         else:
             query_list.append(query)
 
+def get_num():
+    print("Please enter the number of images you want from all of the searches (num>=1)")
+    while(True):
+        num = input("Enter your number: ")
+        if(num >= 1):
+            return num
+        else:
+            continue
+
+
+
+def get_URLs(query_list, num_images, driver, directory):
+
+    driver.get("https://www.google.com/imghp?hl=en&tab=wi&authuser=0&ogbl")
+    for query in query_list:
+        url = []
+        print("Getting images under: " + query_list[num])
+        print()
+
+        #Preform search query 
+        search = driver.find_element_by_name('q')
+        search.send_keys(query)
+        search.send_keys(Keys.RETURN)
+        time.sleep(5)
+
+        #Ensure proper number of images are loaded
+        image = driver.find_elements_by_css_selector("img.Q4LuWd") 
+        print("Found " + str(len(image)))
+        time.sleep(1)
+        while len(image) < num_images:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            image = driver.find_elements_by_css_selector("img.Q4LuWd")
+            time.sleep(1)
+            print("Found " + str(len(image)))
+
+        #Add Image URL's to url list 
+        for num in range(num_images):   
+            url.append(image[num].get_attribute('src'))
+        
+        #Retrieve images from URL list for the Query 
+        retrieve_Images(url, query, directory)
+
+
+def retrieve_Images(url, query, directory):
+    
+    counter = 0
+    #Iterate through URL list to pull out images
+    for num in range(len(url)):
+        #Case 1: Image stored in base64
+        try:
+            print(url[num])
+            url_string = url[num].split(',')[1]
+            
+            #print(url_string)
+            file_name = query + "_" + str(counter) + '.png'
+            #save_Images(url_string, file_name, directory, 1)
+            with open(file_name, "wb") as fh:
+                fh.write(base64.b64decode(url_string))
+            print(file_name)
+        
+        except:
+            print("Error:" + query + " " + str(num) + " not stored in Base64 formatting. Attepmting to pull a request for the image.")
+            #Case 2: Image does not exist in base64. Request the image from the webpage. 
+            try: 
+                driver.get(url[num])
+                image = driver.find_element_by_tag_name('img')
+                src = image.get_attribute('src')
+                file_name = query + "_" + str(counter) + '.png'
+                img = requests.get(src)
+                #save_Images(img, file_name, directory, 0)
+                with open(file_name, "wb") as fh:
+                    fh.write(img.content)
+                print(file_name)
+            except:
+                #Handling case of if image URL returned as Null
+                if url[num] is None:
+                    continue
+                #Handling case if image URL request failed
+                else:
+                    print("Failed to reach url: " + url[num])
+                    print(url[num])
+                    continue
+        time.sleep(0.5) #Rest period to avoid blockage by Google
+        counter += 1
+    print("Saved " + str(counter) + " " + query + " Images")
+    print()
+    
+
+def save_Images(img, file_name, directory, base_64_bin):
+    if base_64_bin == 1:
+       with open(file_name, "wb") as fh:
+            fh.write(base64.b64decode(img))
+        #print(file_name)
+    else:
+        with open(file_name, "wb") as fh:
+            fh.write(img.content)
+        #print(file_name)
+
+                   
 if __name__ == '__main__':
+    PATH = '/mnt/c/Users/bc234/Coding_Repo/Web_Scrape/chromedriver.exe'
+    
+    driver = webdriver.Chrome(PATH)
+
+    
     print("Welcome to the Not a Virus Google Image Scraper. Remember to run this program with Administrator privalleges.")
 
+    #Get needed information 
     query_list = get_queueries()
+    num_images = get_num()
+    #print("Please enter the directory you would like to add the pictures. Use the formate: ")
+    directory = ""
 
-    for i in query_list:
-        print(i)
-        print()
+    #Begin process of getting Images
+    get_URLs(query_list, num_images, driver, directory)
+    
+
+    driver.quit()
