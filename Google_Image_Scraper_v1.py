dd06a90bdcc796d99b8156242a73ad0744d95a4a
Ben-Costa <bc234cc@gmail.comm>
2020-08-05 12:00:30 -0500

    Final working version of Cascade version before final merge with main

--- Google_Image_Scraper_v1.py
+++ Google_Image_Scraper_v1.py
@@ -40,10 +40,7 @@ def get_dicrectory():
     directory = input("Enter your directory: ")
     return directory
 
-
-
-
-def get_URLs(query_list, num_images, driver):
+def get_URLs(query_list, num_images, driver, directory):
 
     driver.get("https://www.google.com/imghp?hl=en&tab=wi&authuser=0&ogbl")
     for query in query_list:
@@ -51,88 +48,101 @@ def get_URLs(query_list, num_images, driver):
         print("Getting images under: " + query)
         print()
 
-        #Preform search query 
+        # Preform search query 
         search = driver.find_element_by_name('q')
         search.send_keys(query)
         search.send_keys(Keys.RETURN)
         time.sleep(5)
 
-        #Ensure proper number of images are loaded
+        # Ensure proper number of images are loaded
         image = driver.find_elements_by_css_selector("img.Q4LuWd") 
-        print("Found " + str(len(image)))
-        time.sleep(1)
+        print("Found " + str(len(image)) + "Images")
+        time.sleep(5)
         while len(image) < num_images:
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             image = driver.find_elements_by_css_selector("img.Q4LuWd")
-            time.sleep(1)
-            print("Found " + str(len(image)))
+            time.sleep(5)
+            print("Found " + str(len(image)) + "Images")
 
-        #Add Image URL's to url list 
+        # Add Image URL's to url list 
         for num in range(num_images):   
             url.append(image[num].get_attribute('src'))
         
-        #Retrieve images from URL list for the Query 
-        retrieve_Images(url, query)
+        # Retrieve images from URL list for the Query 
+        retrieve_Images(url, query, directory)
         image.clear()
         url.clear()
         driver.find_element_by_name('q').clear()
+        time.sleep(5)
 
-def retrieve_Images(url, query):
+def retrieve_Images(url, query, directory):
     
     counter = 0
-    #Iterate through URL list to pull out images
+    # Iterate through URL list to pull out images
     for num in range(len(url)):
-        #Case 1: Image stored in base64
+        # Case 1: Image stored in base64
         try:
-            #print(url[num])
             url_string = url[num].split(',')[1]
-            
-            #print(url_string)
             file_name = query + "_" + str(counter) + '.png'
-            #save_Images(url_string, file_name, directory, 1)
-            with open(file_name, "wb") as fh:
-                fh.write(base64.b64decode(url_string))
-            print(file_name)  
+            save_Images(url_string, file_name, directory, 1, query) 
         except:
             print("Error:" + query + " " + str(num) + " not stored in Base64 formatting. Attepmting to pull a request for the image.")
-            #Case 2: Image does not exist in base64. Request the image from the webpage. 
+            # Case 2: Image does not exist in base64. Request the image from the webpage. 
             try: 
                 driver.get(url[num])
                 image = driver.find_element_by_tag_name('img')
                 src = image.get_attribute('src')
                 file_name = query + "_" + str(counter) + '.png'
                 img = requests.get(src)
-                #save_Images(img, file_name, directory, 0)
-                with open(file_name, "wb") as fh:
-                    fh.write(img.content)
-                print(file_name)
+                save_Images(img, file_name, directory, 0, query)
             except:
                 #Handling case of if image URL returned as Null
                 if url[num] is None:
                     continue
-                #Handling case if image URL request failed
+                # Handling case if image URL request failed
                 else:
                     print("Failed to reach url: " + url[num])
-                    print(url[num])
                     continue
-        time.sleep(0.5) #Rest period to avoid blockage by Google
+        time.sleep(0.5) # Rest period time. Can be changed to speed up process. 
         counter += 1
     print("Saved " + str(counter) + " " + query + " Images")
     print()
     
-def save_Images(img, file_name, base_64_bin):
+def save_Images(img, file_name, directory, base_64_bin, query):
     
-    if base_64_bin == 1:
-       with open(file_name, "wb") as fh:
-            fh.write(base64.b64decode(img))
-        #print(file_name)
+    # Case 1: If a directory to save was not given select the working directory
+    if(directory == ""):
+        folder_path = os.path.join(os.getcwd(), query)
+        # Check if directory path exists
+        if(not os.path.exists(folder_path)):
+            os.makedirs(folder_path)
+        folder_path = os.path.join(folder_path, file_name)
+        # Case 1: Base64 conversion
+        if base_64_bin == 1:
+            with open(folder_path, "wb") as fh:
+                fh.write(base64.b64decode(img))
+        # Case 2: Request conentet 
+        else:
+            with open(folder_path, "wb") as fh:
+                fh.write(img.content)
+    # Case 2: Directory was given
     else:
-        with open(file_name, "wb") as fh:
-            fh.write(img.content)
-        #print(file_name)
-                  
+        folder_path = os.path.join(directory, query)
+        # Check if directory path exists
+        if(not os.path.exists(folder_path)):
+            os.makedirs(folder_path)
+        folder_path = os.path.join(folder_path, file_name)
+            # Case 1: Base64 conversion
+        if base_64_bin == 1:
+            with open(folder_path, "wb") as fh:
+                fh.write(base64.b64decode(img))
+        # Case 2: Request conentet 
+        else:
+            with open(file_name, "wb") as fh:
+                fh.write(img.content)
+
 if __name__ == '__main__':
-    PATH = ''#place path chrome_driver.exe here in your/path/here format
+    PATH = '/mnt/c/Users/bc234/Coding_Repo/Web_Scrape/chromedriver.exe'#place path chrome_driver.exe here in your/path/here format
     
     driver = webdriver.Chrome(PATH)
    
@@ -144,6 +154,7 @@ if __name__ == '__main__':
     directory = get_dicrectory()
 
     #Begin process of getting Images
-    get_URLs(query_list, num_images, driver)
+    get_URLs(query_list, num_images, driver, directory)
     
+    print("Process Complete")
     driver.quit()
